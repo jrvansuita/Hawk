3aa6ee07fb20e6465b9b767bd3f0db64a45ea145
jrvansuita <vansuita.jr@gmail.com>
2018-04-09 11:45:24 -0300

    teste

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ var app = express();
 app.set('port', process.env.PORT || 5000);
 
 // set up a route to redirect http to https if it's production
-if (process.env.NODE_ENV)
-  app.use(function(req, res, next) {
-    console.log(req.secure);
-    console.log(req.protocol.toString());
-
-    if (!req.protocol.includes('https')) {
-      return res.redirect(['https://', req.get('Host'), req.url].join(''));
-    }
-    next();
-  });
+// if (process.env.NODE_ENV)
+//   app.use(function(req, res, next) {
+//     console.log(req.secure);
+//     console.log(req.protocol.toString());
+//
+//     if (!req.protocol.includes('https')) {
+//       return res.redirect(['https://', req.get('Host'), req.url].join(''));
+//     }
+//     next();
+//   });
 
 app.set('view engine', 'ejs');
 app.use('/css', express.static('css'));
@@ -42,6 +42,8 @@ app.get(['/', '/invoice', '/invoice/overview'], (req, res) => {
 
 
 app.get('/invoice/by-date', (req, res) => {
+  console.log(req.secure);
+  console.log(req.protocol.toString());
 
   var from = req.query.from ? new Date(parseInt(req.query.from)) : Dat.firstDayOfMonth();
   var to = req.query.to ? new Date(parseInt(req.query.to)).maxTime() : Dat.lastDayOfMonth();
