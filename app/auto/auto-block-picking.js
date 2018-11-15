2e980a6393b0b827a738451f6fdb707a33d3c142
jrvansuita <vansuita.jr@gmail.com>
2018-11-15 05:17:06 -0200

    teste

--- app/auto/auto-block-picking.js
+++ app/auto/auto-block-picking.js
@@ -1,33 +1,45 @@
 const PickingLaws = require('../laws/picking-laws.js');
 const SaleLoader = require('../loader/sale-loader.js');
 const BlockedLaws = require('../laws/blocked-laws.js');
-var User = require('../bean/user.js');
+const User = require('../bean/user.js');
 
 module.exports= class AutoBlockPicking {
 
   constructor(){
-
+    this.sales = PickingLaws.getFullList();
   }
 
-  onProduct(product){
-    if (product._Estoque.estoqueDisponivel > 0){
-      BlockedLaws.toggleBlock(product.codigo, new User(404), 994);
+  onProduct(products, index, onTerminate){
+    if (index < products.length){
+      var product = products[index];
+
+console.log('Testou ' + product.codig);
+
+      if (product._Estoque.estoqueDisponivel < 1){
+        ///nao pode bloquead o que já está bloqueado
+        BlockedLaws.toggleBlock(product.codigo, new User(404), '994', ()=>{
+          index++;
+          onProduct(products, index);
+        });
+      }
+    }else{
+      onTerminate();
     }
   }
 
- onSale(sale){
-   new SaleLoader(sale).loadProducts((products)=>{
-      product.forEach((product)=>{
-        if (product._Estoque.estoqueDisponivel > 0){
-
-        }
+  onSale(sales, index){
+    new SaleLoader(this.sales[index]).loadProducts((products)=>{
+      console.log('Carregou os produtos ' + this.sales[index].numeroPedido);
+      onProduct(products, 0, ()=>{
+        index++;
+        onSale(sales, index);
       });
-   }).run();
+    }).run();
 
- }
+  }
 
- run(){
-
- }
+  run(){
+    onSale(this.sales, 0);
+  }
 
 };
