9300b404bf6d4175aeee54d94d5a7986f529b56c
Junior Vansuita <jrvansuita@Dracon.local>
2020-01-03 10:07:03 -0300

    mundi

--- sandbox.js
+++ sandbox.js
@@ -6,28 +6,14 @@ const Initilizer = require('./app/abra-cadabra/initializer.js');
 new Initilizer(__dirname, true).begin(() => {
 
 
-  const MagentoCalls = require('./app/magento/magento-calls.js');
 
+const job = require('./app/jobs/job-mundipagg-checker.js');
 
-  /*new MagentoCalls().updateProductWeight('CB606ro-P', 0.1460).then((result) => {
-    console.log(result);
-  })*/
+const EccosysProvider = require('./app/eccosys/eccosys-provider.js');
 
-
-
-new MagentoCalls().salesOrderUpdate({
-    orderIncrementId: '120838374',
-    status:           'pending_payment',
-    comment:          'teste',
-    notify:           false
-  }).then((r) => {
-    console.log(r);
-  });
-
- /*new MagentoCalls().product('PG012br-1').then((product) => {
-   console.log(product);
- })*/
-
+new EccosysProvider().sale('729422').go((sale) => {
+  new job().handleSale(sale);
+})
 
 
 
