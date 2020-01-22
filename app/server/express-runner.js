bb56dabf0a1524512dd88fda21d09c048e660210
juniorvansuita <Vansuita1324>
2020-01-21 23:07:29 -0300

    s

--- app/server/express-runner.js
+++ app/server/express-runner.js
@@ -48,7 +48,9 @@ var allowedOrigins = [
 
 
 var allowCorsMiddleware = (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", allowedOrigins.join(';'));
+  if(allowedOrigins.indexOf(req.headers.origin) > -1){
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
   res.header('Access-Control-Allow-Methods', 'GET');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
