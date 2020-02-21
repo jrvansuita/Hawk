b2eb44e1c8dbb6da1f2d337222a8f566a04002a2
Junior Vansuita <jrvansuita@Dracon.local>
2020-02-21 15:56:37 -0300

    data sample template

--- app/bean/template.js
+++ app/bean/template.js
@@ -6,18 +6,20 @@ module.exports = class Template extends DataAccess {
     this.subject = Str.def(subject);
     this.content = Str.def(content);
     this.type = Str.def(type);
+    this.sample = {};
   }
 
   static getKey() {
     return ['_id'];
   }
 
+  static findByType(type, callback){
+    Template.findOne({type:type}, callback);
+  }
 
-
- static findByType(type, callback){
-   Template.findOne({type:type}, callback);
- }
-
+  static updateSample(id, data){
+    Template.upsert({_id: id}, {sample : data});
+  }
 
   static delete(id, callback){
     Template.findOne({_id:id}, (err, obj)=>{
