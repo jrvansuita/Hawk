70cbdec42d66c1001c1be5ecee9bfe4e081e7c80
Junior Vansuita <jrvansuita@Dracon.local>
2020-03-31 21:33:46 -0300

    sss

--- app/jobs/job-sync-sales.js
+++ app/jobs/job-sync-sales.js
@@ -51,7 +51,7 @@ module.exports = class JobSyncSales extends Job{
       })
       .pageCount(1000)
       //.dates(Dat.yesterday(), Dat.yesterday())
-      .dates(new Date('03-13-2019'), Dat.today())
+      .dates(new Date('01-01-2020'), Dat.today())
       .doneSales()
       .pagging()
       .each((salePage, nextPage)=>{
