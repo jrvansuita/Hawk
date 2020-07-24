2e65929873c593ad06dd647a0cf70e88f0ab100b
jrvansuita <vansuita.jr@gmail.com>
2020-07-24 14:52:41 -0300

    market prep

--- src/server/midlewares.js
+++ src/server/midlewares.js
@@ -3,26 +3,26 @@ const File = require('../file/file.js')
 const buffer = require('buffer/').Buffer
 
 module.exports = class ServerMidlewares {
-  constructor (express) {
+  constructor(express) {
     this.express = express
     this.userLoader = require('../provider/user-provider.js')
   }
 
-  attach () {
+  attach() {
     this.defaultRoutes()
     this.wellcome()
     this.errors()
     this.routes()
   }
 
-  defaultRoutes () {
+  defaultRoutes() {
     const routeStack = require('express').Router()
 
     // Generic Rules
     routeStack.use('*', this.getGenericRouteRule())
 
-    // Api and Admin Rules
-    routeStack.use('/admin/*', this.getAdminAuthRouteRule())
+    // Api and Market Rules
+    routeStack.use('/market/*', this.getMarketAuthRouteRule())
     routeStack.use('/api/*', this.getApiAuthRouteRule())
 
     // Web Backend Rules
@@ -32,7 +32,7 @@ module.exports = class ServerMidlewares {
     this.express.use(routeStack)
   }
 
-  wellcome () {
+  wellcome() {
     this.express.get(['/'], (req, res) => {
       if (req.session.lastpath && req.session.lastpath !== '/') {
         res.redirect(req.session.lastpath)
@@ -42,11 +42,13 @@ module.exports = class ServerMidlewares {
     })
   }
 
-  getRoutes () {
-    return new File('src/routes').getFolderFilesPaths('.js').filter((e) => { return !e.startsWith('_') })
+  getRoutes() {
+    return new File('src/routes').getFolderFilesPaths('.js').filter((e) => {
+      return !e.startsWith('_')
+    })
   }
 
-  routes () {
+  routes() {
     this.getRoutes().forEach((file) => {
       var Clazz = require('../routes/' + file)
 
@@ -54,25 +56,25 @@ module.exports = class ServerMidlewares {
     })
   }
 
-  getAdminAuthRouteRule () {
+  getMarketAuthRouteRule() {
     return (req, res, next) => {
       next('router')
     }
   }
 
   /**
-    * @api {post} /* Authentication
-    * @apiGroup Credentials
-    * @apiDescription All api calls must have this parameters setted
-    * @apiHeaderExample {json} Header-Example:
-    *  {
-    *   "access": "User Access ID"
-    *   "pass": "User Password"
-    *   "appkey": "App Key"
-    * }
- */
+   * @api {post} /* Authentication
+   * @apiGroup Credentials
+   * @apiDescription All api calls must have this parameters setted
+   * @apiHeaderExample {json} Header-Example:
+   *  {
+   *   "access": "User Access ID"
+   *   "pass": "User Password"
+   *   "appkey": "App Key"
+   * }
+   */
 
-  getApiAuthRouteRule () {
+  getApiAuthRouteRule() {
     return (req, res, next) => {
       try {
         req.headers.device = req.headers.device || 'api'
@@ -88,7 +90,7 @@ module.exports = class ServerMidlewares {
     }
   }
 
-  getLoginRedirectRouteRule () {
+  getLoginRedirectRouteRule() {
     return (req, res, next) => {
       res.locals.loggedUser = req.session.loggedUser
 
@@ -108,7 +110,7 @@ module.exports = class ServerMidlewares {
     }
   }
 
-  getPostCheckUserRouteRule () {
+  getPostCheckUserRouteRule() {
     return (req, res, next) => {
       if (req.session.loggedUserID || req.path === '/login') {
         res.locals.loggedUser = req.session.loggedUser
@@ -119,7 +121,7 @@ module.exports = class ServerMidlewares {
     }
   }
 
-  getGenericRouteRule (req, res, next) {
+  getGenericRouteRule(req, res, next) {
     return (req, res, next) => {
       // console.log(`${req.method}: ${req.originalUrl}`)
       res.locals.query = req.query
@@ -129,7 +131,7 @@ module.exports = class ServerMidlewares {
     }
   }
 
-  errors () {
+  errors() {
     const History = require('../bean/history.js')
     process.on('uncaughtException', function (err) {
       console.log(err)
@@ -139,7 +141,7 @@ module.exports = class ServerMidlewares {
     })
   }
 
-  _checkApiKeyCall (req) {
+  _checkApiKeyCall(req) {
     var appKey = global.Arr.find(Params.apiAppKeys(), req.headers.appkey)
 
     if (appKey) {
