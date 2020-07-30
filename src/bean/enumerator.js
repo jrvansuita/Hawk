9fe2a86ed17edae26a487d1aa43f5ed83b5f372e
juniorvansuita <Vansuita1324>
2020-07-30 15:16:26 -0300

    fixes

--- src/bean/enumerator.js
+++ src/bean/enumerator.js
@@ -1,92 +1,99 @@
-
-var cache = {}
+var cache = {};
 
 module.exports = class Enumerator extends DataAccess {
-  constructor (name, explanation, tag, items) {
-    super()
-    this.id = global.Util.id()
-    this.name = global.Str.def(name)
-    this.explanation = global.Str.def(explanation)
-    this.tag = global.Str.def(tag)
-    this.items = items || []
+  constructor(name, explanation, tag, items) {
+    super();
+    this.id = global.Util.id();
+    this.name = global.Str.def(name);
+    this.explanation = global.Str.def(explanation);
+    this.tag = global.Str.def(tag);
+    this.items = items || [];
 
     // Every Item
     // -- icon, description, name, value, default:true||false}
   }
 
-  static getKey () {
-    return ['id']
+  static getKey() {
+    return ['id'];
   }
 
-  setItems (items) {
-    this.items = items
-    return this
+  static clearCache() {
+    cache = {};
   }
 
-  mapBy (mapProp) {
-    this.mapProp = mapProp
-    return this
+  setItems(items) {
+    this.items = items;
+    return this;
   }
 
-  async _get () {
+  mapBy(mapProp) {
+    this.mapProp = mapProp;
+    return this;
+  }
+
+  async _get() {
     return new Promise((resolve, reject) => {
       if (cache[this.tag]) {
-        resolve(cache[this.tag])
+        resolve(cache[this.tag]);
       } else {
         Enumerator.findOne({ tag: this.tag }, (_err, data) => {
-          cache[this.tag] = data
-          resolve(cache[this.tag])
-        })
+          cache[this.tag] = data;
+          resolve(cache[this.tag]);
+        });
       }
-    })
+    });
   }
 
-  async _getMap () {
-    return this.get().then((data) => {
+  async _getMap() {
+    return this.get().then(data => {
       return data?.items?.reduce((o, item) => {
-        o[item[this.mapProp || 'value']] = item
-        return o
-      }, {})
-    })
+        o[item[this.mapProp || 'value']] = item;
+        return o;
+      }, {});
+    });
   }
 
-  static delete (id, callback) {
+  static delete(id, callback) {
     this.findOne({ id: id }, (_err, obj) => {
-      if (obj) { obj.remove() }
+      if (obj) {
+        obj.remove();
+      }
 
-      callback()
-    })
+      callback();
+    });
   }
 
-  static duplicate (id, callback) {
+  static duplicate(id, callback) {
     this.findOne({ id: id }, (_err, obj) => {
       new Enumerator('[Duplicado] ' + obj.name, obj.explanation, 'Nenhuma', obj.items).upsert((_err, doc) => {
-        callback(doc)
-      })
-    })
+        callback(doc);
+      });
+    });
   }
 
-  static on (tag, useDef) {
-    var e = new Enumerator()
-    e.tag = tag
-    e.useDef = useDef
-    return e
+  static on(tag, useDef) {
+    var e = new Enumerator();
+    e.tag = tag;
+    e.useDef = useDef;
+    return e;
   }
 
-  async get (mapped) {
-    return mapped ? this._getMap() : this._get()
+  async get(mapped) {
+    return mapped ? this._getMap() : this._get();
   }
 
-  async hunt (value, prop = 'name') {
-    return this.get().then((data) => {
-      var def
-      var r = data?.items?.find((each) => {
-        def = each.default ? each : def
-        return each?.[prop]?.split(',')?.some((part) => {
-          return part.trim() === value.trim()
-        }) || (this.useDef ? def : null)
-      })
-      return r
-    })
+  async hunt(value, prop = 'name') {
+    return this.get().then(data => {
+      var def;
+      var r = data?.items?.find(each => {
+        def = each.default ? each : def;
+        return (
+          each?.[prop]?.split(',')?.some(part => {
+            return part.trim() === value.trim();
+          }) || (this.useDef ? def : null)
+        );
+      });
+      return r;
+    });
   }
-}
+};
--- src/vault/enumerator-vault.js
+++ src/vault/enumerator-vault.js
@@ -1,29 +1,25 @@
-
-const Enumerator = require('../bean/enumerator.js')
+const Enumerator = require('../bean/enumerator.js');
 
 module.exports = class {
-  static storeFromScreen (params, callback) {
-    var object = new Enumerator(
-      params.name,
-      params.explanation,
-      params.tag,
-      params.items
-    )
+  static storeFromScreen(params, callback) {
+    var object = new Enumerator(params.name, params.explanation, params.tag, params.items);
 
     if (params.id) {
-      object.id = params.id
+      object.id = params.id;
     }
+
+    Enumerator.clearCache();
 
     if (params.id) {
       Enumerator.findOne({ id: params.id }, (_err, obj) => {
         object.upsert((_err, doc) => {
-          callback(doc ? doc.id : 0)
-        })
-      })
+          callback(doc ? doc.id : 0);
+        });
+      });
     } else {
       Enumerator.create(object, (_err, doc) => {
-        callback(doc ? doc.id : 0)
-      })
+        callback(doc ? doc.id : 0);
+      });
     }
   }
-}
+};
